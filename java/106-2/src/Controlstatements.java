adee64e34db29cb423ec177ec0497a1c3e6c5477
chengzi516 <p772307283@outlook.com>
2023-01-19 21:51:53 +0800

    java

---
+++ java/106-2/src/Controlstatements.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 13378343877
+ * Date: 2023-01-19
+ * Time: 12:55
+ */
+public class Controlstatements {
+    public static void main(String[] args) {
+//        int a=1,b=2,c=1,d=1;
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//        if(1)
+//        {
+//            System.out.println(d);
+//        }
+//        if(a==2)
+//        {
+//            System.out.println(111);
+//        }
+//        else
+//        {
+//            System.out.println(222);
+//        }
+//        String day="Monday";
+//        if(day=="Monday")
+//        {
+//            System.out.println("it's Monday");
+//        }
+//        else if(day =="Tuesday")
+//        {
+//            System.out.println("it's Tuesday");
+//        }
+//        else
+//        {
+//            System.out.println("it's not Monday or Tuesday");
+//        }
+//        int a=3;
+//        if(a!=3)
+//            System.out.println(111);
+//            System.out.println(222);
+//         int a=1;
+//         switch(a)
+//         {
+//             case 1:
+//                 System.out.println(1);
+//             case 2:
+//                 System.out.println(1);
+//             case 3:
+//                 System.out.println(1);
+//             case 4:
+//                 System.out.println(1);
+//         }
+
+
+//        int a=1;
+//        while(a<4)
+//        {
+//            System.out.println(a);
+//            a++;
+//        }
+//        int a=1;
+//        while(a<5)
+//        {
+//            System.out.println(a);
+//            a++;
+//            if(a==3)
+//                break;
+//        }
+
+//        int a=1;
+//        for (int i = 0; i <10; i++) {
+//            System.out.println(a);
+//            if(a==5)
+//            {
+//                continue;
+//            }
+//            a++;
+//        }
+
+//        for (int i = 0; i <10 ; ++i) {
+//            System.out.print(i);
+//        }
+//        int x=1;
+//        System.out.println(111);
+//        System.out.print(222);
+//        System.out.printf("%d",x);
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("输入名字");
+//        String name=sc.nextLine();
+//        System.out.println("输入年龄");
+//        int age=sc.nextInt();
+//        //进行输出
+//        System.out.println(name);
+//        System.out.println(age);
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNextInt())
+        {
+            int a=sc.nextInt();
+        }
+    }
+}
