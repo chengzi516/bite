ab6fa9a888bd1399a4bf4e14b15efa0814f89190
chengzi516 <p772307283@outlook.com>
2023-03-09 19:23:58 +0800

    practice

---
+++ practise/leetcode/Project9/源.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+
+void quick_sort(int* arr, int l, int r) {
+    if (l >= r)
+        return;
+    int i = l - 1;
+    int j = r + 1;
+    int x = arr[(l + r >> 1)];
+    while (i < j) {
+        do { i++; } while (arr[i] < x);
+        do { j--; } while (arr[j] > x);
+        if (i < j)
+        {
+            int tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
+
+    }
+    quick_sort(arr, l, j);
+    quick_sort(arr, j + 1, r);
+}
+
+int main() {
+    int n = 0;
+    scanf("%d", &n);
+    int arr[1000];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    quick_sort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+}
